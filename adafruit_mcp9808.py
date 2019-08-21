a315641a1d0b6c8a27ebe9d7e6cdceb543e9645a
Scott Shawcroft <scott@tannewt.org>
2019-08-21 15:43:56 -0700

    Remove stop kwarg and use write_then_readinto.
    See https://github.com/adafruit/circuitpython/issues/2082 for details.

--- adafruit_mcp9808.py
+++ adafruit_mcp9808.py
@@ -75,16 +75,16 @@ class MCP9808:
         self.buf = bytearray(3)
         self.buf[0] = 0x06
         with self.i2c_device as i2c:
-            i2c.write(self.buf, end=1, stop=False)
-            i2c.readinto(self.buf, start=1)
+            i2c.write_then_readinto(self.buf, self.buf,
+                                    out_end=1, in_start=1)
 
         ok = self.buf[2] == 0x54 and self.buf[1] == 0
 
         # Check device id.
         self.buf[0] = 0x07
         with self.i2c_device as i2c:
-            i2c.write(self.buf, end=1, stop=False)
-            i2c.readinto(self.buf, start=1)
+            i2c.write_then_readinto(self.buf, self.buf,
+                                    out_end=1, in_start=1)
 
         if not ok or self.buf[1] != 0x04:
             raise ValueError("Unable to find MCP9808 at i2c address " + str(hex(address)))
@@ -94,8 +94,8 @@ class MCP9808:
         """Temperature in celsius. Read-only."""
         self.buf[0] = 0x05
         with self.i2c_device as i2c:
-            i2c.write(self.buf, end=1, stop=False)
-            i2c.readinto(self.buf, start=1)
+            i2c.write_then_readinto(self.buf, self.buf,
+                                    out_end=1, in_start=1)
 
         # Clear flags from the value
         self.buf[1] = self.buf[1] & 0x1f
