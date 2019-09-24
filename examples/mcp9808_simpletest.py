5d20bb22c043e2709db982f4c0fa16ce33fe4b3c
signalmeister <45778744+signalmeister@users.noreply.github.com>
2019-09-24 21:19:46 +0100

    Update mcp9808_simpletest.py
    The original example code did not show the syntax for setting up a module with anything other than the default address. While this information is to be found in the object definition document, a simple amendment to the sample code gives the default and an amended address example.

--- examples/mcp9808_simpletest.py
+++ examples/mcp9808_simpletest.py
@@ -5,7 +5,11 @@ import adafruit_mcp9808
 
 # This example shows how to get the temperature from a MCP9808 board
 i2c_bus = busio.I2C(board.SCL, board.SDA)
-mcp = adafruit_mcp9808.MCP9808(i2c_bus)
+mcp = adafruit_mcp9808.MCP9808(i2c_bus) # Object has only one parameter specified so default address (0x18) is used
+
+# If the address of the module has been changed (eg by connecting A0 to VDD making address 0x19)
+# mcp = adafruit_mcp9808.MCP9808(i2c_bus,0x19) # Object now has two parameters - the new address 0x19 being the second one
+
 
 while True:
     tempC = mcp.temperature
