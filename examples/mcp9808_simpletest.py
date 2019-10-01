1caf93f7ab09c4d9a9f04a9eaca8779f8e3084c6
signalmeister <45778744+signalmeister@users.noreply.github.com>
2019-10-01 19:20:58 +0100

    Update mcp9808_simpletest.py
    I am not really sure that I understood your comments being a total novice at this process - I do not know what a lint check being unhappy means.
    If there are still issues, can you please let me know and I hope I can understand your special terms.

--- examples/mcp9808_simpletest.py
+++ examples/mcp9808_simpletest.py
@@ -3,12 +3,14 @@ import board
 import busio
 import adafruit_mcp9808
 
-# This example shows how to get the temperature from a MCP9808 board
+# This example shows how to get the temperature from a MCP9808 board.
+# The object has only one parameter specified so default address (0x18) is used
 i2c_bus = busio.I2C(board.SCL, board.SDA)
-mcp = adafruit_mcp9808.MCP9808(i2c_bus) # Object has only one parameter specified so default address (0x18) is used
+mcp = adafruit_mcp9808.MCP9808(i2c_bus)
 
-# If the address of the module has been changed (eg by connecting A0 to VDD making address 0x19)
-# mcp = adafruit_mcp9808.MCP9808(i2c_bus,0x19) # Object now has two parameters - the new address 0x19 being the second one
+# This second example if for when the address of the module has been changed (eg by connecting A0 to VDD making address=0x19)
+# mcp = adafruit_mcp9808.MCP9808(i2c_bus,0x19)
+# Object now has two parameters - the new address (0x19) being the second one
 
 
 while True:
