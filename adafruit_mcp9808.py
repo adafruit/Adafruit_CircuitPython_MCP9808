a86abf8c9059f69d2f11e4a05ad6bde0e945f8ee
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_mcp9808.py
+++ adafruit_mcp9808.py
@@ -41,7 +41,7 @@ from adafruit_bus_device.i2c_device import I2CDevice
 from adafruit_register.i2c_bits import RWBits
 from adafruit_register.i2c_bit import ROBit
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MCP9808.git"
 
 
